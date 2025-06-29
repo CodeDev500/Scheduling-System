b108969d41c8633d0b3b87bb7a8e5f9c01722b0d
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-06-30 00:58:48 +1200

    jsonwebtoken handler

--- server/src/utils/jwtHandler.ts
+++ server/src/utils/jwtHandler.ts
@@ -1,11 +1,34 @@
 import jwt from "jsonwebtoken";
+import { Response } from "express";
+import { User } from "@prisma/client";
+require("dotenv").config();
 
-type TPayload = { id: string };
+type TPayload = { user: User };
 
 export const generateToken = (payload: TPayload): string => {
-  return jwt.sign(payload, process.env.SECRET_KEY!, { expiresIn: "1d" }); // 1 day
+  return jwt.sign(payload, process.env.ACCESS_TOKEN!, { expiresIn: "1h" });
 };
 
-export const verifyToken = (token: string): TPayload => {
-  return jwt.verify(token, process.env.SECRET_KEY!) as TPayload;
+export const generateRefreshToken = (payload: TPayload): string => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN!, { expiresIn: "1d" });
+};
+
+export const setTokens = (res: Response, user: User): string => {
+  const accessToken = generateToken({ user });
+  const refreshToken = generateRefreshToken({ user });
+
+  res.cookie("accessToken", accessToken, {
+    // httpOnly: true,
+    maxAge: 60 * 60 * 1000, // 1 hour in milliseconds
+    // secure: true,
+    // sameSite: "none",
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    // secure: process.env.NODE_ENV === "production",
+    // sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
+  });
+
+  return accessToken;
 };
